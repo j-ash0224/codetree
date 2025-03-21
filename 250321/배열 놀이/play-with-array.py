ba9409ff67de714ca115c490d8ef823ebd9ec10a
PJ <121143497+j-ash0224@users.noreply.github.com>
2025-03-21 18:20:35 +0900

    Title: 배열 놀이, Time: 47ms, Memory: 16MB, Status: Passed - CodeTree

--- 250321/배열 놀이/play-with-array.py
+++ 250321/배열 놀이/play-with-array.py
@@ -8,12 +8,17 @@ for _ in range(q):
     if qureyList[0] ==1:
         print(lista[qureyList[1]-1])
     elif qureyList[0] ==2:
+        have=False
         for i in range(n):
             if lista[i] == qureyList[1]:
                 print(i+1)
+                have=True
                 break
+        if have==False:
+            print('0')
     elif qureyList[0] ==3:
         for i in range(qureyList[1], qureyList[2]+1):
             print(lista[i-1], end=' ')
         print()
+    
         
